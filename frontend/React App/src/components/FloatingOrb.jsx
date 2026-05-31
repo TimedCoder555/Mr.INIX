702cd7ff1561226e00402190c09cb7f59ec778a0
TimedCoder555 <matrixebay0110@gmail.com>
2026-05-31 10:32:30 +0600

    Update FloatingOrb.jsx

--- frontend/React App/src/components/FloatingOrb.jsx
+++ frontend/React App/src/components/FloatingOrb.jsx
@@ -1,1 +1,16 @@
-.
+import React from "react";
+
+function FloatingOrb() {
+  return (
+    <div className="orb-wrapper">
+      <div className="orb-ring ring-1"></div>
+      <div className="orb-ring ring-2"></div>
+
+      <div className="orb-core">
+        <span>INIX</span>
+      </div>
+    </div>
+  );
+}
+
+export default FloatingOrb;
