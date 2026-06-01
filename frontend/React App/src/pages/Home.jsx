5b7c01832e0911cdeb5dad52d4282abd4541fd49
TimedCoder555 <matrixebay0110@gmail.com>
2026-06-01 09:02:13 +0600

    Update Home.jsx

--- frontend/React App/src/pages/Home.jsx
+++ frontend/React App/src/pages/Home.jsx
@@ -1,1 +1,32 @@
-.
+import React from "react";
+
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import FloatingOrb from "../components/FloatingOrb";
+import SuggestionCards from "../components/SuggestionCards";
+import ChatBox from "../components/ChatBox";
+import VoiceButton from "../components/VoiceButton";
+
+function Home() {
+  return (
+    <div className="home-page">
+
+      <Navbar />
+
+      <Hero />
+
+      <FloatingOrb />
+
+      <SuggestionCards />
+
+      <div className="home-actions">
+        <VoiceButton />
+      </div>
+
+      <ChatBox />
+
+    </div>
+  );
+}
+
+export default Home;
