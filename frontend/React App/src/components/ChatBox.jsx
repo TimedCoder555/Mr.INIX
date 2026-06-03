5734de19c7b67e4d2064cb70527e0b9634860c6a
TimedCoder555 <matrixebay0110@gmail.com>
2026-06-03 12:24:16 +0600

    Update ChatBox.jsx

--- frontend/React App/src/components/ChatBox.jsx
+++ frontend/React App/src/components/ChatBox.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { sendMessage } from "./utils/api";
+import { sendMessage } from "../utils/api";
 
 export default function ChatBox() {
   const [messages, setMessages] = useState([
